7e9cfa539075e9f94cf32dd62652585e9f94f78e
karolmbednarski <karol.m.bednarski@gmail.com>
2025-12-15 19:21:03 +0100

    cleaning

--- v2/+FA3/+sparse/+block/dynamic_4.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = dynamic_4(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(73)=log(T(45)-T(46))-params(30)*T(12)^(1+params(4))/(1+params(4))+params(1)*y(112);
-end
--- v2/+FA3/+sparse/+block/static_2.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = static_2(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(5)=(-1.09861229866811);
-end
